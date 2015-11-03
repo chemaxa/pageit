be3c6d469c0e769484165c93c7ba1694f51f0bb6
Maxim <chemaxa@gmail.com>
2015-11-03 14:25:55 +0300

    ready

--- jquery.pageit.js
+++ jquery.pageit.js
@@ -1,120 +1,134 @@
 (function($) {
     'use strict';
-    var settings = {
-        height: 400,
-        fadeSpeed: 400,
-        showPagination: true,
-        nextText: 'Вперед',
-        prevText: 'Назад',
-        el: {},
-        lastPage: 1,
-    };
-    var methods = {
-        // initialization function
-        init: function(options) {
-            //Copy settings  
-            $.extend(settings, options);
-            settings.el = this;
-            console.log(methods.getPaginationTemplate(this), methods.getPaginationEl(this));
-            var fullPages = [],
-                pageElements = [],
-                height = 0;
-
-            // Summ heights of all content element childs
-            this.children().each(function(_) {
-                //console.log(this, this.clientHeight);
-                // if height greater then needed, create page
-                if (height + this.clientHeight > settings.height) {
-                    fullPages.push(pageElements);
-                    pageElements = [];
+    $.fn.pageIt = function(method) {
+        var settings = {
+            height: 400,
+            fadeSpeed: 400,
+            nextText: 'Вперед',
+            prevText: 'Назад',
+            el: {},
+            lastPage: 1,
+            autoHeight: false,
+            paginatePages: 0,
+            showNumpages: false
+        };
+        var methods = {
+            // initialization function
+            init: function(options) {
+                var elem = document.querySelector(this.selector);
+                if (options.autoHeight) {
+                    if (elem.getBoundingClientRect().bottom > window.innerHeight)
+                        settings.height = window.innerHeight * 0.7;
+                }
+                //Copy settings  
+                $.extend(settings, options);
+                settings.el = this;
+                var fullPages = [],
+                    pageElements = [],
                     height = 0;
+                // If elem height < needle height than return
+
+                if (this.outerHeight() < settings.height) return;
+
+
+                // Summ heights of all content element childs
+                this.children().each(function() {
+                    //console.log(this, this.clientHeight);
+                    // if height greater then needed, create page
+                    if (height + this.offsetHeight > settings.height) {
+                        fullPages.push(pageElements);
+                        pageElements = [];
+                        height = 0;
+                    }
+
+                    height += this.offsetHeight;
+                    pageElements.push(this);
+                });
+
+                // tails
+                if (height > 0) {
+                    fullPages.push(pageElements);
+                }
+                // If only 1 page
+                console.log(fullPages);
+                if (fullPages.length == 1) return;
+
+                // Append paginator to element
+                this.append(methods.getPaginationTemplate());
+
+
+                // wrapping each full page
+                $(fullPages).wrap("<div class='page'></div>");
+
+                // hiding all wrapped pages
+                this.children('.page').hide();
+
+                // making collection of pages for pagination
+                settings.paginatePages = this.children('.page');
+
+                // draw controls
+                if (settings.showNumpages)
+                    methods.showNumpages($(settings.paginatePages).length);
+
+                // show first page
+                methods.showPage(settings.paginatePages, settings.lastPage);
+                // Click on pagination links, if Delegate target has page_num attribute, then go to page 
+                // and binding 2 events - on clicking to Prev & next
+                $(settings.el).on('click.pageIt', function(el) {
+                    if (!el.target.dataset) return;
+                    if (el.target.dataset.pg == 'prev')
+                        methods.showPage(settings.paginatePages, settings.lastPage);
+
+                    if (el.target.dataset.pg == 'next')
+                        methods.showPage(settings.paginatePages, settings.lastPage + 2);
+
+                    if (el.target.dataset.pg_num)
+                        methods.showPage(settings.paginatePages, el.target.dataset.page_num);
+                });
+
+            },
+            // update counter function
+            updateCounter: function(i) {
+                methods.getPaginationEl(settings.el).find('#page_number').html(i);
+            },
+
+            // show page function
+            showPage: function(pageList, pageNum) {
+                var i = pageNum - 1;
+                if (pageList[i]) {
+                    // hiding old page, display new one
+                    $(pageList[settings.lastPage]).fadeOut(
+                        settings.fadeSpeed,
+                        function() {
+                            settings.lastPage = i;
+                            $(pageList[settings.lastPage]).fadeIn(settings.fadeSpeed);
+                        }
+                    );
+
+                    // and updating counter
+                    methods.updateCounter(pageNum);
+
+                    methods.getPaginationEl(settings.el).find('.page_n').removeClass('pgActive');
+                    methods.getPaginationEl(settings.el).find('.page_n').eq(pageNum - 1).addClass('pgActive');
+                }
+            },
+
+            // show pagination function (draw switching numbers)
+            showNumpages: function(numPages) {
+                var pagins = '';
+                for (var i = 1; i <= numPages; i++) {
+                    pagins += '<a href="#" class="page_n" data-pg_num=' + i + '>' + i + '</a>';
                 }
 
-                height += this.clientHeight;
-                pageElements.push(this);
-            });
-
-            // tails
-            if (height > 0) {
-                fullPages.push(pageElements);
-            }
-            // If only 1 page
-            if (fullPages.length == 1) return;
-            if (settings.showPagination || this.height() > $(window).height()) {
-                this.append(methods.getPaginationTemplate(this));
-            }
-
-            // wrapping each full page
-            $(fullPages).wrap("<div class='page'></div>");
-
-            // hiding all wrapped pages
-            this.children().hide();
-
-            // making collection of pages for pagination
-            //paginatePages = this.children();
-
-            // draw controls
-            //methods.showPagination($(paginatePages).length);
-
-            // show first page
-            methods.showPage(this.children(), settings.lastPage);
-            // Click on pagination links, if Delegate target has page_num attribute, then go to page 
-            // and binding 2 events - on clicking to Prev & next
-            $(window).on('click.pageIt', function(el) {
-                if (!el.target.dataset) return;
-                if (el.target.dataset.pg == 'prev')
-                    methods.showPage(settings.lastPage);
-
-                if (el.target.dataset.pg == 'next')
-                    methods.showPage(settings.lastPage + 2);
-
-                if (el.target.dataset.pg_num)
-                    methods.showPage(el.target.dataset.page_num);
-            });
-        },
-        // update counter function
-        updateCounter: function(i) {
-            methods.getPaginationEl(settings.el).find('#page_number').html(i);
-        },
-
-        // show page function
-        showPage: function(pageList, pageNum) {
-            var i = pageNum - 1;
-            if (pageList[i]) {
-                // hiding old page, display new one
-                $(pageList[settings.lastPage]).fadeOut(
-                    settings.fadeSpeed,
-                    function() {
-                        settings.lastPage = i;
-                        $(pageList[settings.lastPage]).fadeIn(settings.fadeSpeed);
-                    }
-                );
-
-                // and updating counter
-                methods.updateCounter(pageNum);
-
-                methods.getPaginationEl(settings.el).find('.page_n').removeClass('act');
-                methods.getPaginationEl(settings.el).find('.page_n').eq(pageNum - 1).addClass('act');
-            }
-        },
-
-        // show pagination function (draw switching numbers)
-        showPagination: function(numPages) {
-            var pagins = '';
-            for (var i = 1; i <= numPages; i++) {
-                pagins += '<li><a href="#" class="page_n" data-pg_num=' + i + '>' + i + '</a></li>';
-            }
-            methods.getPaginationTemplate.find('li:first-child').after(pagins);
-        },
-        getPaginationTemplate: function() {
-            return '<ul class="pgPagination"><li><a href = "#" data-pg = "prev" class = "pgPrev" > ' + settings.prevText + '</a></li><li><a href = "#" data-pg = "next"  class = "pgNext"> ' + settings.nextText + ' </a></li></ul>';
-        },
-        getPaginationEl: function(el) {
-            return el.siblings('.pgPagination');
-        },
-    };
-
-    $.fn.pageIt = function(method) {
+                settings.el.find('.pgPagination a:first-child').after(pagins);
+            },
+            getPaginationTemplate: function() {
+                return '<div class="pgPagination"><a href="#" data-pg = "prev" class = "pgPrev" > ' + settings.prevText + '</a><a href="#" data-pg = "next"  class = "pgNext"> ' + settings.nextText + ' </a></div>';
+            },
+            getPaginationEl: function(el) {
+                return el.children('.pgPagination');
+            },
+        };
         if (methods[method]) {
             return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
         } else if (typeof method === 'object' || !method) {
@@ -124,4 +138,5 @@
         }
         return this;
     };
+
 })(jQuery);
